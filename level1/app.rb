674d849991432351b5bc613f20a7fc0592c085b4
jordi Tabit <jordi.tabit@hotmail.fr>
2019-10-26 11:52:38 +0200

    First level completed

--- level1/app.rb
+++ level1/app.rb
@@ -1,3 +1,85 @@
-require "json"
+# frozen_string_literal: true
 
-# votre code
+require "json"
+require "date"
+
+# Prices calculator service
+class PricesCalculator
+  attr_reader :communications, :practitioners
+
+  COMMUNICATION_BASE_PRICE = 0.10
+  ADDITIONAL_PAGE_PRICE = 0.07
+  COLOR_MODE_PRICE = 0.18
+  EXPRESS_DELIVERY_PRICE = 0.60
+
+  def initialize(data)
+    @communications = data["communications"]
+    @practitioners = data["practitioners"]
+
+    validate!
+  end
+
+  def run
+    compute_data
+  end
+
+  private
+
+  def compute_data
+    daily_revenue = communications.each_with_object({}) do |communication, total|
+      stringified_date = Date.parse(communication["sent_at"]).strftime('%F')
+      if total[stringified_date]
+        total[stringified_date] += total_price_for(communication)
+      else
+        total[stringified_date] = total_price_for(communication)
+      end
+    end
+    format_data(daily_revenue)
+  end
+
+  def format_data(data)
+    output = data.map do |date, total|
+      { "sent_on" => date, "total" => total.round(2) }
+    end
+    { "totals" => output }
+  end
+
+  def find_practitioner(id)
+    practitioners.find { |practitioner| practitioner["id"] == id }
+  end
+
+  def additional_page_price_for(communication)
+    raise ArgumentError, "Attribute pages_number can't be negative" if communication["pages_number"].negative?
+
+    (communication["pages_number"] - 1) * ADDITIONAL_PAGE_PRICE
+  end
+
+  def color_mode_price_for(communication)
+    communication["color"] ? COLOR_MODE_PRICE : 0
+  end
+
+  def express_delivery_price_for(communication)
+    practitioner = find_practitioner(communication["practitioner_id"])
+    practitioner["express_delivery"] ? EXPRESS_DELIVERY_PRICE : 0
+  end
+
+  def total_price_for(communication)
+    COMMUNICATION_BASE_PRICE +
+      additional_page_price_for(communication) +
+      color_mode_price_for(communication) +
+      express_delivery_price_for(communication)
+  end
+
+  def validate!
+    raise ArgumentError, "Wrong input format" if practitioners.nil? || communications.nil?
+  end
+end
+
+## Example of implementation
+data = JSON.parse(File.read("data.json"))
+
+daily_revenue = PricesCalculator.new(data).run
+
+File.open("./output.json", "w") do |f|
+  f.write(JSON.pretty_generate(daily_revenue))
+end
--- level1/app_spec.rb
+++ level1/app_spec.rb
@@ -1,12 +1,119 @@
+# frozen_string_literal: true
+
 # rspec app_spec.rb
 
 require 'rspec'
 
 require_relative './app.rb'
 
-RSpec.describe "Prices calculator", type: :model do
-  let(:data)          { JSON.parse(File.read('data.json')) }
-  let(:output)        { JSON.parse(File.read('output.json')) }
+RSpec.describe PricesCalculator, type: :service do
+  let(:data) { JSON.parse(File.read('data.json')) }
+  let(:date) { "2019-03-01 00:00:00" }
+  let(:basic_data) do
+    { "practitioners" =>
+        [{
+          "id" => 1,
+          "first_name" => "Simple",
+          "last_name" => "Practitioner",
+          "express_delivery" => false
+        }],
+      "communications" =>
+        [{
+          "id" => 1,
+          "practitioner_id" => 1,
+          "pages_number" => 1,
+          "color" => false,
+          "sent_at" => date
+        }] }
+  end
 
-  pending("calculates prices")
+  describe "prices" do
+    context "for one communication" do
+      it "costs 10 cents when no option" do
+        daily_revenue = described_class.new(basic_data).run
+
+        expect(daily_revenue["totals"].first["total"])
+          .to eq(0.10)
+      end
+
+      it "costs 18 cents more when color mode" do
+        basic_data["communications"].first["color"] = true
+
+        daily_revenue = described_class.new(basic_data).run
+
+        expect(daily_revenue["totals"].first["total"])
+          .to eq(0.10 + 0.18)
+      end
+
+      it "costs 7 cents more per additionnal page" do
+        basic_data["communications"].first["pages_number"] = 5
+
+        daily_revenue = described_class.new(basic_data).run
+
+        expect(daily_revenue["totals"].first["total"])
+          .to eq(0.10 + 4 * 0.07)
+      end
+
+      it "cost 60 cents more if the author has the express delivery option" do
+        basic_data["practitioners"].first["express_delivery"] = true
+
+        daily_revenue = described_class.new(basic_data).run
+
+        expect(daily_revenue["totals"].first["total"])
+          .to eq(0.10 + 0.60)
+      end
+    end
+
+    context "for many communications" do
+      let(:another_communication) do
+        {
+          "id" => 2,
+          "practitioner_id" => 1,
+          "pages_number" => 1,
+          "color" => false,
+          "sent_at" => nil
+        }
+      end
+
+      it "adds up communications prices when sent on same day" do
+        another_communication["sent_at"] = date
+        basic_data["communications"] << another_communication
+
+        daily_revenue = described_class.new(basic_data).run
+
+        expect(daily_revenue["totals"].first["total"])
+          .to eq(0.10 + 0.10)
+      end
+
+      it "divides communications prices when sent on different days" do
+        another_date = "2019-03-02 00:00:00"
+        another_communication["sent_at"] = another_date
+        basic_data["communications"] << another_communication
+
+        daily_revenue = described_class.new(basic_data).run
+
+        expect(daily_revenue["totals"].size).to eq(2)
+        expect(daily_revenue["totals"].first["total"])
+          .to eq(0.10)
+      end
+    end
+  end
+
+  describe "data format" do
+    context "with wrong formatted input data" do
+      it "raises an error" do
+        wrong_data = { 'foo': 'bar' }
+
+        expect { described_class.new(wrong_data).run }
+          .to raise_error(ArgumentError, "Wrong input format")
+      end
+    end
+
+    it "raises an error if a communication has a negative number of pages" do
+      data["communications"].first["pages_number"] = -1
+
+      expect { described_class.new(data).run }
+        .to raise_error(ArgumentError, "Attribute pages_number can't be negative")
+    end
+  end
 end
